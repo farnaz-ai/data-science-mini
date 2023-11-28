7a84c334717b7c1a3365a0c727e331475eca87b1
farnaz-ai <75072957+farnaz-ai@users.noreply.github.com>
2023-11-28 12:07:37 +0330

    Update Multiple_Regression.py

--- Multiple_Regression.py
+++ Multiple_Regression.py
@@ -1,4 +1,10 @@
 import pandas
+from sklearn import linear_model
 df=pandas.read_csv('data.csv')
-x= df[[]]
-y= df['']
+x= df[['name','date']]
+y= df['time']
+
+regr = linear_model.LinearRegression()
+regr.fit(x,y)
+
+predicttime = regr.predict([[, ]])
