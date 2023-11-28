87aca99a488e0660ac526f5755fedd5b89f75c0d
farnaz-ai <75072957+farnaz-ai@users.noreply.github.com>
2023-11-28 12:11:49 +0330

    Update Multiple_Regression.py
    this will predict at what time will the employee no 551 enter the office on 1402/09/04

--- Multiple_Regression.py
+++ Multiple_Regression.py
@@ -7,4 +7,6 @@ y= df['time']
 regr = linear_model.LinearRegression()
 regr.fit(x,y)
 
-predicttime = regr.predict([['0000000517', ]])
+predicttime = regr.predict([['0000000517',1402/09/04' ]])
+
+print(predicttime)
